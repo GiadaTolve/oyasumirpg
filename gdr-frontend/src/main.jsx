b900d7edaac928a9128cdabcd40309fb7d437f3a
Giada Giuliana Tolve <tolvegiada@gmail.com>
2025-09-21 12:53:50 +0200

    fix main.jsx

--- gdr-frontend/src/main.jsx
+++ gdr-frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- 1. Assicurati che sia importato
 import App from './App.jsx';
 import './index.css';
 import { SocketProvider } from './SocketContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SocketProvider>
-      <App />
-    </SocketProvider>
+    <BrowserRouter> {/* <-- 2. Deve avvolgere tutto */}
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </BrowserRouter> {/* <-- 3. E chiudersi qui */}
   </React.StrictMode>,
 )
